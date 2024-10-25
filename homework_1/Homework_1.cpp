e0fd1b1787316af2f55b9e6342226041f275a0a8
Mehmet Bedir Sevimli <mehmetbedirsevimli123@gmail.com>
2024-10-25 16:54:15 +0300

    Homework_1 added.

---
+++ homework_1/Homework_1.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+    string timeInput;
+
+    char colon; // Will be used to capture ':' character
+
+    // Definition for time structure
+    struct timeStruct {
+        int hours;
+        int minutes;
+        int seconds;
+        long total_seconds;
+    };
+
+    while (1) {
+
+        cout << "Please enter the time (HH:MM:SS format): ";
+
+        cin >> timeInput;
+
+        stringstream timeStream(timeInput);
+
+        timeStruct time1;
+
+        // Parsing the string to get time values
+        timeStream >> time1.hours >> colon;
+        timeStream >> time1.minutes >> colon;
+        timeStream >> time1.seconds;
+
+        // Checking if hours, minutes, and seconds are valid
+
+        if ((time1.hours >= 24) || (time1.hours < 0)) {
+            cout << "The entered hour value " << time1.hours << " is incorrect." << endl;
+            continue;
+        }
+        if ((time1.minutes >= 60) || (time1.minutes < 0)) {
+            cout << "The entered minute value " << time1.minutes << " is incorrect." << endl;
+            continue;
+        }
+        if ((time1.seconds >= 60) || (time1.seconds < 0)) {
+            cout << "The entered second value " << time1.seconds << " is incorrect." << endl;
+            continue;
+        }
+
+        // If stringstream fails (invalid format entered)
+
+        if (timeStream.fail()) {
+            cout << "Invalid time format entered!" << endl;
+            continue;
+        }
+
+        // If everything is valid, calculate the total seconds
+        long total_seconds = time1.hours * 3600 + time1.minutes * 60 + time1.seconds;
+        cout << "Total value in seconds: " << total_seconds << endl;
+
+        // Break the loop after a valid input
+        break;
+    }
+}
---
+++ homework_1/README.md
@@ -0,0 +1,42 @@
+﻿
+
+INTRODUCTION TO PROGRAMMING
+
+HOMEWORK - 1
+
+Create a struct called time.  Let 
+this struct contain four variables of type **int hour**, **int minute**, **int second** and **long total\_second**. 
+
+Enter the hours, minutes, seconds from the keyboard in the format 12:59:59 and store them in a variable of type **struct Time**. 
+
+**NOTE: Check that the entered hour, minute, second values are greater than 60 and less than 0.** 
+
+Store the time struct type variable entered in the main program in the variable total\_seconds by calculating the seconds according to the formula below. 
+
+Total seconds formula: 
+
+long totalsecs= t1.hour\*3600+t1.minute\*60+t1.second //t1 variable is considered as struct time type.
+
+NOTE: variable names will not be a,b,c,x,y,z,t1,t2 etc.
+
+**In order for your assignment to be evaluated, you must have made the same screen output as below. Do not try to process differently.**
+
+Sample screen output :
+
+Enter the time in Hours:Minutes:Seconds: 12:34:05
+
+Values calculated in seconds: 45245
+
+NOTE: Values not entered as Seconds:Minutes:Seconds will not be accepted. Do not enter the hour, enter the minute, enter the second.
+
+Sample screen output :
+
+Enter the time in Hours:Minutes:Seconds : 55:104:505
+
+the entered time value 55 has been entered incorrectly.
+
+the entered minute value 104 has been entered incorrectly.
+
+the entered second value 505 was entered incorrectly.
+
+End of PROGRAM. 
